2f1c05714b8b9b8f8ad0dca0b54b0525872e5ab0
eddgrace <35617907+eddgrace@users.noreply.github.com>
2025-05-24 09:11:03 +0800

    Update CreatePocEnv.sh

--- scripts/Postgres/CreatePocEnv.sh
+++ scripts/Postgres/CreatePocEnv.sh
@@ -7,6 +7,9 @@ Drop sequence trx_serial;
 CREATE SEQUENCE trx_serial START 1;
 Create table trx_data ( id int, tx_time timestamp);
 
+ALTER TABLE trx_data
+ADD CONSTRAINT trx_data_pkey PRIMARY KEY (id);
+
 select * from trx_data;
 
 EOF
